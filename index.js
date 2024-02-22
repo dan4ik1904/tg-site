750b1c58722a6107d6576259501e47c874536eb4
dan4ik1904 <danyaldobro12345678@gmail.com>
2024-02-22 17:31:32 +0300

    adding files

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express')
+const Bot = require('node-telegram-bot-api')
+
+const token = '7097107639:AAGMDm8AvjqtjpmZXkmfIX-2ZmRVAypIz28';
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new Bot(token, {polling: true});
+
+// Matches "/echo [whatever]"
+bot.on('message', (msg) => {
+  bot.sendMessage(msg.chat.id, msg.text);
+});
 
 const app = express()
 
-const PORT = evn.PORT || 8080
+const PORT = 8080
 
 app.listen(PORT, () => {
     console.log('bot sterted on PORT', PORT)
